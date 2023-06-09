2013a96e03c6786cf6895b441943785b7071d777
Ethan_Vodka <gohho1008@gmail.com>
2023-06-10 05:05:24 +0900

    プロジェクト ファイルを追加します。

---
+++ Views/PassWordForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Utils.Views
+{
+    partial class PassWordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PnlTitle = new System.Windows.Forms.Panel();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.LblTitle = new System.Windows.Forms.Label();
+            this.LblContent = new System.Windows.Forms.Label();
+            this.TxbPassword = new System.Windows.Forms.TextBox();
+            this.TxbConfirm = new System.Windows.Forms.TextBox();
+            this.LblConfirm = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.PboxMain = new System.Windows.Forms.PictureBox();
+            this.PnlTitle.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PboxMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PnlTitle
+            // 
+            this.PnlTitle.BackColor = System.Drawing.Color.DimGray;
+            this.PnlTitle.Controls.Add(this.LblTitle);
+            this.PnlTitle.Controls.Add(this.BtnExit);
+            this.PnlTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PnlTitle.Location = new System.Drawing.Point(0, 0);
+            this.PnlTitle.Name = "PnlTitle";
+            this.PnlTitle.Size = new System.Drawing.Size(381, 23);
+            this.PnlTitle.TabIndex = 0;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExit.ForeColor = System.Drawing.Color.White;
+            this.BtnExit.Location = new System.Drawing.Point(357, 1);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(20, 20);
+            this.BtnExit.TabIndex = 1;
+            this.BtnExit.Text = "X";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            // 
+            // LblTitle
+            // 
+            this.LblTitle.AutoSize = true;
+            this.LblTitle.Font = new System.Drawing.Font("游ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.LblTitle.ForeColor = System.Drawing.Color.White;
+            this.LblTitle.Location = new System.Drawing.Point(3, 2);
+            this.LblTitle.Name = "LblTitle";
+            this.LblTitle.Size = new System.Drawing.Size(88, 16);
+            this.LblTitle.TabIndex = 2;
+            this.LblTitle.Text = "Authentication";
+            // 
+            // LblContent
+            // 
+            this.LblContent.AutoSize = true;
+            this.LblContent.Font = new System.Drawing.Font("游ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.LblContent.ForeColor = System.Drawing.Color.Black;
+            this.LblContent.Location = new System.Drawing.Point(111, 47);
+            this.LblContent.Name = "LblContent";
+            this.LblContent.Size = new System.Drawing.Size(165, 16);
+            this.LblContent.TabIndex = 3;
+            this.LblContent.Text = "please enter your password :";
+            // 
+            // TxbPassword
+            // 
+            this.TxbPassword.Location = new System.Drawing.Point(114, 66);
+            this.TxbPassword.Name = "TxbPassword";
+            this.TxbPassword.Size = new System.Drawing.Size(236, 19);
+            this.TxbPassword.TabIndex = 4;
+            // 
+            // TxbConfirm
+            // 
+            this.TxbConfirm.Location = new System.Drawing.Point(114, 114);
+            this.TxbConfirm.Name = "TxbConfirm";
+            this.TxbConfirm.Size = new System.Drawing.Size(236, 19);
+            this.TxbConfirm.TabIndex = 6;
+            // 
+            // LblConfirm
+            // 
+            this.LblConfirm.AutoSize = true;
+            this.LblConfirm.Font = new System.Drawing.Font("游ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.LblConfirm.ForeColor = System.Drawing.Color.Black;
+            this.LblConfirm.Location = new System.Drawing.Point(111, 95);
+            this.LblConfirm.Name = "LblConfirm";
+            this.LblConfirm.Size = new System.Drawing.Size(177, 16);
+            this.LblConfirm.TabIndex = 5;
+            this.LblConfirm.Text = "please confirm your password :";
+            // 
+            // btnOK
+            // 
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.ForeColor = System.Drawing.Color.Black;
+            this.btnOK.Location = new System.Drawing.Point(248, 146);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(51, 20);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCancel.ForeColor = System.Drawing.Color.Black;
+            this.BtnCancel.Location = new System.Drawing.Point(318, 146);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(51, 20);
+            this.BtnCancel.TabIndex = 7;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // PboxMain
+            // 
+            this.PboxMain.Location = new System.Drawing.Point(6, 47);
+            this.PboxMain.Name = "PboxMain";
+            this.PboxMain.Size = new System.Drawing.Size(92, 86);
+            this.PboxMain.TabIndex = 8;
+            this.PboxMain.TabStop = false;
+            // 
+            // PassWordForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(381, 178);
+            this.Controls.Add(this.PboxMain);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.TxbConfirm);
+            this.Controls.Add(this.LblConfirm);
+            this.Controls.Add(this.TxbPassword);
+            this.Controls.Add(this.LblContent);
+            this.Controls.Add(this.PnlTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PassWordForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PassWordForm";
+            this.PnlTitle.ResumeLayout(false);
+            this.PnlTitle.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PboxMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlTitle;
+        private System.Windows.Forms.Label LblTitle;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Label LblContent;
+        private System.Windows.Forms.TextBox TxbPassword;
+        private System.Windows.Forms.TextBox TxbConfirm;
+        private System.Windows.Forms.Label LblConfirm;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.PictureBox PboxMain;
+    }
+}
